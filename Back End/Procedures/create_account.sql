5835e6024f678630d03737d6ec8b8390847a1de7
roshan-del <60114587+roshan-del@users.noreply.github.com>
2021-04-08 16:24:10 -0600

    Add files via upload

--- Back End/Procedures/create_account.sql
+++ Back End/Procedures/create_account.sql
@@ -1,28 +1,28 @@
 CREATE PROCEDURE `create_account` (
-    IN _name
-    IN _password
-    IN _email
-    IN _doc
-    IN _address
-    IN _cert
-    IN _shop
+    IN `_name` varchar(30),
+    IN `_password` varchar(50),
+    IN `_email` varchar(50),
+    IN `_doc` varchar(20),
+    IN `_address` varchar(255),
+    IN `_cert` varchar(255),
+    IN `_shop` varchar(255)
 )
 BEGIN
 
-INSERT INTO account( id, password, email, date_of_creation)
-VALUES(_name, _password, _email, _doc)
+INSERT INTO `account`
+VALUES(_name, _password, _email, _doc);
 
 IF _address is NOT NULL
 
-INSERT INTO mechanic(id, address) VALUES (_name, _address)
+INSERT INTO `mechanic` VALUES (_name, _address)
 
-IF _cert and _shop is NOT NULL
+IF _cert AND _shop is NOT NULL
 
-INSERT INTO professional(id, certification, shop_name) VALUES (_name, _cert, _shop)
+INSERT INTO `professional` VALUES (_name, _cert, _shop);
 
-IF _cert and _shop is NULL
+IF _cert AND _shop is NULL
 
-INSERT INTO amateur(id) VALUES (_name)
+INSERT INTO `amateur`(id) VALUES (_name);
 
-END
+END;
 
